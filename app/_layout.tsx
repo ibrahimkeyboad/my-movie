06a40b73d69f51710e687f8435eb9c78d255b7c4
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-03-09 18:10:07 +0300

    update: enhance RootLayout with additional Stack screens for navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,10 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
