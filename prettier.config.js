15b71056547fe33a92ab826e1ec23bfdcdddd78b
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-03-09 18:11:54 +0300

    feat: add TypeScript interfaces for Movie and TrendingMovie data structures

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  printWidth: 100,
-  tabWidth: 2,
-  singleQuote: false,
-  bracketSameLine: true,
+  // printWidth: 100,
+  // tabWidth: 2,
+  // singleQuote: false,
+  // bracketSameLine: true,
   // trailingComma: 'es5',
 
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
